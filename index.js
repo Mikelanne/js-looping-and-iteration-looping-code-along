161a1b61551c30d3cbba9e14b48ffd3d8546c0b5
Mikelanne <mikelanne.wood@gmail.com>
2021-03-14 16:07:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(friends, event) {
+    let cards = [];
+
+    for (let i = 0; i < friends.length; i++) {
+       cards.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+    };
+    return cards
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
